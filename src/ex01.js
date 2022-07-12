d3fa29bf9bcd2ceae2cd9fb12b5baee514eae9b4
Ha young, Kim <jeremiah91@naver.com>
2022-07-12 18:08:52 +0900

    chore : cleanup

--- src/ex01.js
+++ src/ex01.js
@@ -1,50 +1,64 @@
 import * as THREE from "three"
 
+// ----- 주제: AxesHelper, GridHelper
+
 export default function example() {
-  const canvas = document.querySelector("#test-canvas")
-  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+  // Renderer
+  const canvas = document.querySelector("#three-canvas")
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  })
   renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1)
 
   // Scene
   const scene = new THREE.Scene()
 
-  // Camera -- PerspectiveCamera
-  // const camera = new THREE.PerspectiveCamera(
-  //   75, // fov
-  //   window.innerWidth / window.innerHeight, // aspect
-  //   0.1, // near
-  //   1000 // far
-  // )
-
-  // camera.position.y = 2
-  // camera.position.z = 5
-  // scene.add(camera)
-
-  // Camera -- OrthographicCamera
-  const camera = new THREE.OrthographicCamera(
-    -(window.innerWidth / window.innerHeight), // left
-    window.innerWidth / window.innerHeight, // right
-    1, // top
-    -1, // bottom
-    0.1, // near
-    1000 // far
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
   )
-  camera.position.x = 2
-  camera.position.y = 2
-  camera.position.z = 10
-  camera.lookAt(0, 0, 0)
-  camera.zoom = 0.5
-  camera.updateProjectionMatrix()
+  camera.position.z = 5
   scene.add(camera)
+
+  const light = new THREE.DirectionalLight(0xffffff, 1)
+  light.position.x = 1
+  light.position.z = 2
+  scene.add(light)
 
   // Mesh
   const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshBasicMaterial({
-    color: "#333",
+  const material = new THREE.MeshStandardMaterial({
+    color: "seagreen",
   })
   const mesh = new THREE.Mesh(geometry, material)
   scene.add(mesh)
 
-  // draw
-  renderer.render(scene, camera)
+  // 그리기
+  const clock = new THREE.Clock()
+
+  function draw() {
+    const time = clock.getElapsedTime()
+
+    mesh.rotation.y = time
+
+    renderer.render(scene, camera)
+    renderer.setAnimationLoop(draw)
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera)
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize)
+
+  draw()
 }
