7874fbf309ac7572c90533370aad8ccce7498d22
Ha young, Kim <jeremiah91@naver.com>
2022-07-12 15:03:47 +0900

    feat : camera

--- src/main.js
+++ src/main.js
@@ -3,3 +3,17 @@ import * as THREE from "three"
 const canvas = document.querySelector("#test-canvas")
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(window.innerWidth, window.innerHeight)
+
+// Scene
+const scene = new THREE.Scene()
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75, // fov
+  window.innerWidth / window.innerHeight, // aspect
+  0.1, // near
+  1000 // far
+)
+
+camera.position.z = 5
+scene.add(camera)
