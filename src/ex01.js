b8e7229cfd19be7ffc9f899ea5f9ed7d3691afef
Ha young, Kim <jeremiah91@naver.com>
2022-07-14 15:14:56 +0900

    feat : geometry control

--- src/ex01.js
+++ src/ex01.js
@@ -23,7 +23,7 @@ export default function example() {
     0.1,
     1000
   )
-  camera.position.z = 4
+  camera.position.z = 10
   scene.add(camera)
 
   // Light
@@ -39,20 +39,44 @@ export default function example() {
   const controls = new OrbitControls(camera, renderer.domElement)
 
   // Mesh
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const geometry = new THREE.SphereGeometry(5, 64, 64)
   const material = new THREE.MeshStandardMaterial({
-    color: "hotpink",
+    color: "pink",
     // wireframe: true,
     side: THREE.DoubleSide,
+    flatShading: true,
   })
   const mesh = new THREE.Mesh(geometry, material)
   scene.add(mesh)
+
+  const positionArray = geometry.attributes.position.array
+  const randomArray = []
+
+  for (let i = 0; i < positionArray.length; i += 3) {
+    positionArray[i] += (Math.random() - 0.5) * 0.2
+    positionArray[i + 1] += (Math.random() - 0.5) * 0.2
+    positionArray[i + 2] += (Math.random() - 0.5) * 0.2
+
+    randomArray[i] = (Math.random() - 0.5) * 0.2
+    randomArray[i + 1] = (Math.random() - 0.5) * 0.2
+    randomArray[i + 2] = (Math.random() - 0.5) * 0.2
+  }
 
   // 그리기
   const clock = new THREE.Clock()
 
   function draw() {
-    const delta = clock.getDelta()
+    const time = clock.getElapsedTime() * 3
+
+    for (let i = 0; i < positionArray.length; i += 3) {
+      positionArray[i] += Math.sin(time + randomArray[i] * 100) * 0.002
+      positionArray[i + 1] += Math.sin(time + randomArray[i] * 100) * 0.002
+      positionArray[i + 2] += Math.sin(time + randomArray[i] * 100) * 0.002
+      // positionArray += Math.sign(time) * 0.001
+      // positionArray[i + 2] += Math.sign(time) * 0.001
+    }
+
+    geometry.attributes.position.needsUpdate = true
 
     renderer.render(scene, camera)
     renderer.setAnimationLoop(draw)
